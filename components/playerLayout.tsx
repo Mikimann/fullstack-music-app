bcd39c4a158ef9c32d6a3de039c0c48c3dd104fb
Mikimann <jurja.dumitru@gmail.com>
2023-02-27 21:37:29 +0200

    Update playerLayout component

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box } from "@chakra-ui/layout";
+import SideBar from "./sidebar";
 
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        Sidebar
+        <SideBar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {children}
