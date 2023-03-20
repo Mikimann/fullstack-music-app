be3c1f124895e7bf5d966ff31401574fa4f72573
Mikimann <jurja.dumitru@gmail.com>
2023-03-20 12:14:58 +0200

    Create authorisation logic file

---
+++ lib/auth.ts
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+// HOFunction that is going to be used as middleware to
+// protect the api routes. API route will be wrapped in this HOF
+// and protected from accesing it unauthorized.
+// Checks for token in the cookie, validity.
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.ACCESS_TOKEN;
+
+    if (token) {
+      let user;
+
+      try {
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+
+        if (!user) {
+          throw new Error("User doesn`t exist");
+        }
+      } catch (error) {
+        res.status(401);
+        res.json({ error: "You`re not authorized" });
+        return;
+      }
+      // Pass the user to the next handler
+      return handler(req, res, user);
+    }
+    // If you don`t have a token to begin with.
+    res.status(401);
+    res.json({ error: "You`re not authorized" });
+  };
+};
