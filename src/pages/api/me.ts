bdfbedf7e767c695c00f7263b06e934f7852b517
Mikimann <jurja.dumitru@gmail.com>
2023-04-03 15:35:34 +0300

    Get playlists count when using useMe hook

--- src/pages/api/me.ts
+++ src/pages/api/me.ts
@@ -1,7 +1,15 @@
 import { validateRoute } from "lib/auth";
+import prisma from "lib/prisma";
 
 // This file gets the user. validateRoute already gets the user, it just sends it back here after wrapping it.
 
-export default validateRoute((req, res, user) => {
-  res.json(user);
+export default validateRoute(async (req, res, user) => {
+  // count() does the same as findMany() but instead of returning what it finds, it returns the count
+  const playlistCount = await prisma.playlist.count({
+    where: {
+      userId: user.id,
+    },
+  });
+  console.log(playlistCount);
+  res.json({ ...user, playlistCount });
 });
