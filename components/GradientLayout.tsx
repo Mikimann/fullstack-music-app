f5829789adafbd9b7f32355c1eeb95def93ac141
Mikimann <jurja.dumitru@gmail.com>
2023-04-04 19:48:37 +0300

    Small css modification

--- components/GradientLayout.tsx
+++ components/GradientLayout.tsx
@@ -14,7 +14,8 @@ const GradientLayout = ({
 }) => {
   return (
     <Box
-      height="89.4vh"
+      // height="89.4vh"
+      height="100%"
       overflowY="auto"
       bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
     >
