7f1271ca00ca1ddda1803f59ccc2a11f30941879
Mikimann <jurja.dumitru@gmail.com>
2023-04-06 13:33:58 +0300

    State management store file using easy peasy, based on redux

---
+++ lib/store.ts
@@ -0,0 +1,15 @@
+import { createStore, action } from "easy-peasy";
+
+export const store = createStore({
+  // state, what to keep track of.
+  activeSongs: [],
+  activeSong: null,
+  // actions to modify the state.
+  changeActiveSongs: action((state: any, payload) => {
+    // immutable operation. When calling this function it sends up the new activeSongs
+    state.activeSongs = payload;
+  }),
+  changeActiveSong: action((state: any, payload) => {
+    state.activeSongs = payload;
+  }),
+});
