aa4f7ab094204816f67ebc0e3dde9d2034667745
Mikimann <jurja.dumitru@gmail.com>
2024-01-06 20:58:13 +0200

    Make gradient layout on home page responsive

--- components/GradientLayout.tsx
+++ components/GradientLayout.tsx
@@ -14,15 +14,19 @@ const GradientLayout = ({
 }) => {
   return (
     <Box
-      height="89.4vh"
+      height={{ base: "auto", md: "89.4vh" }}
       // height="100%"
       overflowY="auto"
       bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
     >
-      <Flex bg={`${color}.600`} padding="40px" align="end">
+      <Flex
+        bg={`${color}.600`}
+        padding={{ base: "20px", md: "40px" }}
+        align="end"
+      >
         <Box padding="20px">
           <Image
-            boxSize="160px"
+            boxSize={{ base: "80px", md: "160px" }}
             boxShadow="2xl"
             src={image}
             borderRadius={roundImage ? "100%" : "3px"}
@@ -32,7 +36,7 @@ const GradientLayout = ({
           <Text fontSize="x-small" fontWeight="bold" casing="uppercase">
             {subtitle}
           </Text>
-          <Text fontSize="6xl">{title}</Text>
+          <Text fontSize={{ base: "4xl", md: "6xl" }}>{title}</Text>
           <Text fontSize="x-small">{description}</Text>
         </Box>
         <Box
